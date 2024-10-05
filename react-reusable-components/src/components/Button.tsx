015379d8aba8e5d3f8d5d8ad997358a20678a42a
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-10-06 02:29:16 +0600

    rest params, refs, ts errors

--- react-reusable-components/src/components/Button.tsx
+++ react-reusable-components/src/components/Button.tsx
@@ -1,21 +1,39 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps, forwardRef } from 'react';
 import cn from '../utils/cn';
 
-// `bg-purple-500 px-3 py-2 rounded-md ${className} ${
-//             variant == 'outline' ? 'border border-purple-500 bg-opacity-10' : ''
-//           }`
-
-const Button = ({ className, variant }) => {
-  const getVariant = (variant) => {
-    switch (variant) {
-      case 'outline':
-        return 'btn-outline';
-      case 'ghost':
-        return 'btn-ghost';
-      default:
-        return 'btn-solid';
-    }
-  };
-  return <button className={cn(getVariant(variant), className)}>Click</button>;
+type TRef = HTMLButtonElement;
+type TButtonOptions = {
+  variant?: TVariant;
 };
+type TButton = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+> &
+  TButtonOptions;
+type TVariant = 'solid' | 'outline' | 'ghost';
+
+const Button = forwardRef<TRef, TButton>(
+  ({ className, variant = 'solid', ...rest }, ref) => {
+    const getVariant = (variant: TVariant) => {
+      switch (variant) {
+        case 'outline':
+          return 'btn-outline';
+        case 'ghost':
+          return 'btn-ghost';
+        default:
+          return 'btn-solid';
+      }
+    };
+    return (
+      <button
+        {...rest}
+        ref={ref}
+        className={cn(getVariant(variant), className)}
+      >
+        Click
+      </button>
+    );
+  }
+);
 
 export default Button;
