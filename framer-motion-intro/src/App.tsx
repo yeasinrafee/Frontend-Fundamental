2f9e4b7e7ae5108d2163cbb2fe85b94578860513
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-10-14 23:54:54 +0600

    File initialization for framer motion

---
+++ framer-motion-intro/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='main'>
+      <div className='loading'>
+        <div className='glass'></div>
+        <div className='box'></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
