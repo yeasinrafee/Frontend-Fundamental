5fdae8c998a1519025fa20961282c3bdb53d0765
yeasinrafee <mdyeasinrafi7@gmail.com>
2024-10-16 23:50:46 +0600

    side route fixed

--- responsive-landing-page/src/components/ui/SideBar/SideBar.tsx
+++ responsive-landing-page/src/components/ui/SideBar/SideBar.tsx
@@ -1,17 +1,49 @@
+import { cn } from '@/lib/utils';
+import { LayoutDashboard } from 'lucide-react';
 import { NavLink } from 'react-router-dom';
 
 const SideBar = () => {
   return (
-    <aside className='bg-light-gray p-5 h-screen sticky top-0 left-0 overflow-auto col-span-2'>
-      <nav className='flex flex-col '>
+    <aside className='bg-light-gray p-4 lg:p-5 h-screen sticky top-0 left-0 overflow-auto col-span-2 '>
+      <nav className='flex flex-col gap-2'>
         <NavLink
-          to='/admin'
-          className='p-3 bg-gray rounded-md hover:bg-dark-gray hover:text-white transition-all truncate'
+          to='/admin/dashboard'
+          className={({ isActive }) =>
+            cn(
+              'p-3 bg-gray rounded-md hover:bg-dark-gray hover:text-white transition-all flex items-center gap-2',
+              { 'bg-dark-gray text-white': isActive }
+            )
+          }
         >
-          Dashboard
+          <LayoutDashboard className='shrink-0' />
+          <span className='truncate'>Dashboard</span>
         </NavLink>
-        <NavLink to='/admin/add-service'>Add Service</NavLink>
-        <NavLink to='/admin/service-list'>Service List</NavLink>
+
+        <NavLink
+          to='/admin/add-service'
+          className={({ isActive }) =>
+            cn(
+              'p-3 bg-gray rounded-md hover:bg-dark-gray hover:text-white transition-all flex items-center gap-2',
+              { 'bg-dark-gray text-white': isActive }
+            )
+          }
+        >
+          <LayoutDashboard className='shrink-0' />
+          <span className='truncate'>Add Service</span>
+        </NavLink>
+
+        <NavLink
+          to='/admin/service-list'
+          className={({ isActive }) =>
+            cn(
+              'p-3 bg-gray rounded-md hover:bg-dark-gray hover:text-white transition-all flex items-center gap-2',
+              { 'bg-dark-gray text-white': isActive }
+            )
+          }
+        >
+          <LayoutDashboard className='shrink-0' />
+          <span className='truncate'>Service List</span>
+        </NavLink>
       </nav>
     </aside>
   );
--- responsive-landing-page/src/route/route.tsx
+++ responsive-landing-page/src/route/route.tsx
@@ -5,7 +5,7 @@ import AddService from '@/pages/admin/AddService';
 import Dashboard from '@/pages/admin/Dashboard';
 import ServiceList from '@/pages/admin/ServiceList';
 import Home from '@/pages/home';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Navigate } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
@@ -28,6 +28,10 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        element: <Navigate to='/admin/dashboard' />,
+      },
+      {
+        path: 'dashboard',
         element: <Dashboard />,
       },
       {
